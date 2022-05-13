1dafcbe1792334f1df0bb2283012d293482d3767
kjakx <k.jak.xg0g11@gmail.com>
2022-05-13 15:10:37 +0000

    no symbol case implemented

--- hack-assembler/src/main.rs
+++ hack-assembler/src/main.rs
@@ -1,4 +1,31 @@
 mod parser;
-fn main() {
-    println!("Hello, world!");
+mod code;
+
+use std::env;
+use std::fs::File;
+use std::io::prelude::*;
+
+fn main() -> std::io::Result<()> {
+    let args: Vec<String> = env::args().collect();
+    if args.len() < 2 { panic!("usage: hasm <filename>.asm"); }
+    
+    let f = File::open(&args[1])?;
+    let mut p = parser::Parser::new(f);
+    
+    while p.has_more_commands() {
+        p.advance();
+        let bit_code: u16 = match p.command_type() {
+            parser::CommandType::A | parser::CommandType::L => {
+                p.symbol().parse::<u16>().unwrap()
+            },
+            parser::CommandType::C => {
+                let c = code::comp(&p.comp());
+                let d = code::dest(&p.dest());
+                let j = code::jump(&p.jump());
+                (0b111 << 13) | (c << 7) | (d << 3) | j
+            }
+        };
+        println!("{:#b}", bit_code);
+    }
+    Ok(())
 }
