368bae3cf5bb15089de44c3dedf3636f4515099c
kjakx <k.jak.xg0g11@gmail.com>
2022-05-13 04:26:12 +0000

    Parser module

---
+++ hack-assembler/src/parser.rs
@@ -0,0 +1,42 @@
+use std::io::{self, BufReader};
+
+enum CommandType {
+    A_COMMAND,
+    C_COMMAND,
+    L_COMMAND,
+}
+
+pub struct Parser {
+    buf: &BufReader
+}
+
+impl Parser {
+
+    pub fn hasMoreCommands {
+
+    }
+
+    pub fn advance {
+
+    }
+
+    pub fn commandType -> CommandType {
+
+    }
+
+    pub fn symbol -> String {
+
+    }
+
+    pub fn dest -> String {
+
+    }
+
+    pub fn comp -> String {
+
+    }
+
+    pub fn jump -> String {
+
+    }
+}
