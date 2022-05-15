12a96d86d2b4d73f483f194813f3a4c9b8d3d627
kjakx <k.jak.xg0g11@gmail.com>
2022-05-15 08:12:45 +0000

    bugfix + write machine code to .hack file

--- hack-assembler/src/main.rs
+++ hack-assembler/src/main.rs
@@ -3,14 +3,22 @@ mod code;
 
 use std::env;
 use std::fs::File;
+use std::path::Path;
 use std::io::prelude::*;
+use std::io::BufWriter;
+use std::fmt::Write as FmtWrite;
 
 fn main() -> std::io::Result<()> {
     let args: Vec<String> = env::args().collect();
-    if args.len() < 2 { panic!("usage: hasm <filename>.asm"); }
+    if args.len() < 2 { panic!("usage: hack-assembler <filename>.asm"); }
     
-    let f = File::open(&args[1])?;
-    let mut p = parser::Parser::new(f);
+    let fin_path = Path::new(&args[1]);
+    let fin = File::open(fin_path)?;
+    let mut p = parser::Parser::new(fin);
+
+    let fout_path = fin_path.with_extension("hack");
+    let fout = File::create(fout_path)?;
+    let mut writer = BufWriter::<File>::new(fout);
     
     while p.has_more_commands() {
         p.advance();
@@ -22,11 +30,14 @@ fn main() -> std::io::Result<()> {
                 let c = code::comp(&p.comp());
                 let d = code::dest(&p.dest());
                 let j = code::jump(&p.jump());
-                (0b111 << 13) | (c << 7) | (d << 3) | j
+                (0b111 << 13) | (c << 6) | (d << 3) | j
             },
             _ => unimplemented!()
         };
-        println!("{:016b}", bit_code);
+        let mut s = String::new();
+        writeln!(&mut s, "{:016b}", bit_code);
+        writer.write(s.as_bytes()).unwrap();
     }
+    writer.flush().unwrap();
     Ok(())
 }
